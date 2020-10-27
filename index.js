b16e536878fd124bd167e7ab8530d7dad4220930
dapper-firmware-5532 <eyesforwardfilms@gmail.com>
2020-10-27 04:19:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-var message = "Happy holidays!"
+var day = "Happy holidays!"
 
-console.log ('${message}')
+console.log('Happy holidays!')
+
+function happyHolidays(){
+  return ("Happy holidays!")
+}
+
+var you  = "you"
+
+console.log('Happy Holidays, ${you!}')
+
+function happyHolidaysTo(){
+  return ("Happy holidays, ${you!}")
+}
+
+var holiday = 'Independence Day'
+
+var name = 'you'
+
+var days = '20'
+
+function happyCustomHolidayTo(){
+  return ("Happy ${holiday}, ${name}!")
+}
+var holiday = "Mother's Day"
+
+function holidayCountdown(){
+  return ("It's ${days} days until ${holiday}!")
+}
